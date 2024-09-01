c8853ad6ff8848185a26b8a09395a628d26aae6b
aathi-aathi <aathivdr2004@gmail.com>
2024-09-01 17:39:49 +0530

    github url added

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,9 +3,10 @@ const Footer= ()=>{
     return(
         <div className='footer'>
              <div className="social-media">
-             <a href='https://wa.me/8270224796?text=urlencodedtext' target='_blank'><i className="fa-brands fa-whatsapp fa-2xl  icon-btn" style={{color:'white'}}></i></a>
+             <a href='https://wa.me/8270224796?text=Hello' target='_blank'><i className="fa-brands fa-whatsapp fa-2xl  icon-btn" style={{color:'white'}}></i></a>
              <a href='https://www.linkedin.com/in/aathish11' target='_blank'><i className="fa-brands fa-linkedin fa-2xl" style={{color:'white'}}></i></a>
              <a href='https://www.instagram.com/itzz.aathi/' target='_blank'><i className="fa-brands fa-instagram fa-2xl" style={{color:'white'}}></i></a>
+             <a href='https://github.com/aathi-aathi' target='_blank'><i className="fa-brands fa-github fa-2xl" style={{color:'white'}}></i></a>
        
        
         
